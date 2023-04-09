528378e91e7bcf830671dc3eb4e6eee70f3bdd56
Jeremy Gregorio <Jergre@csu.fullerton.edu>
2023-04-09 12:31:06 -0700

    upload images

--- Website.py
+++ Website.py
@@ -1,7 +1,7 @@
 import sqlite3
 from flask import Flask, render_template, request, redirect, url_for, session, abort, current_app
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder = 'images')
 
 @app.route('/')
 @app.route('/blog', methods =['GET'])
@@ -15,7 +15,10 @@ def link():
 
     cur.execute(get_data)
     retrieve_data = cur.fetchall()
-    return render_template('welcome.html', retrieve_data =retrieve_data)
+
+    #file_path = '/Desktop/FullyHacks'
+    image_files = ["/images/image1.jpg", "/images/image2.jpg", "/images/image3.jpg"]
+    return render_template('welcome.html', retrieve_data = retrieve_data, image_files = image_files)
 
 
 if __name__ =="__main__":
